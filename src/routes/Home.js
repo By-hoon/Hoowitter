62f0aa43afa04c6152a1907f0d4e8d56eb6d68b7
연훈 배 <sbaru1997@naver.com>
2021-08-11 17:47:38 +0900

    Cleaning code

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { dbService, storageService } from "fBase";
 import Hooweet from "components/Hooweet";
+import HooweetFactory from "components/HooweetFactory";
 
 const Home = ({ userObj }) => {
-    const [hooweet, setHooweet] = useState("");
     const [hooweets, setHooweets] = useState([]);
-    const [attachment, setAttachment] = useState("");
     useEffect(() => {
         dbService.collection("hooweets").onSnapshot((snapshot) => {
             const hooweetArray = snapshot.docs.map((doc) => ({
@@ -16,66 +14,9 @@ const Home = ({ userObj }) => {
             setHooweets(hooweetArray);
         });
     }, []);
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        let attachmentUrl = "";
-        if (attachment !== "") {
-            const attachmentRef = storageService
-                .ref()
-                .child(`${userObj.uid}/${uuidv4()}`);
-            const response = await attachmentRef.putString(attachment, "data_url");
-            attachmentUrl = await response.ref.getDownloadURL();
-        }
-        const hooweetObj = {
-            text: hooweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-            attachmentUrl,
-        };
-        await dbService.collection("hooweets").add(hooweetObj);
-        setHooweet("");
-        setAttachment("");
-    };
-    const onChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setHooweet(value);
-    };
-    const onFileChange = (event) => {
-        const {
-            target: { files },
-        } = event;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const {
-                currentTarget: { result },
-            } = finishedEvent;
-            setAttachment(result);
-        };
-        reader.readAsDataURL(theFile);
-    };
-    const onClearAttachment = () => setAttachment(null);
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={hooweet}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="What's on your mind?"
-                    maxLength={120}
-                />
-                <input type="file" accept="image/*" onChange={onFileChange} />
-                <input type="submit" value="Hooweet" />
-                {attachment && (
-                    <div>
-                        <img src={attachment} width="50px" height="50px" />
-                        <button onClick={onClearAttachment}>Clear</button>
-                    </div>
-                )}
-            </form>
+            <HooweetFactory userObj={userObj} />
             <div>
                 {hooweets.map((hooweet) => (
                     <Hooweet
